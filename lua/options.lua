fbeeed7782c6de3c526b44b1762be19416fdb1f1
wmhess <bill@wmhess.com>
2022-06-16 16:12:42 -0400

    lua-2 commit 1
    A better fresh start. Just the structure with mappings, options, plugins directories. Only 2 plugins at this point: Packer, Lualine.

---
+++ lua/options.lua
@@ -0,0 +1,3 @@
+local opt = vim.opt -- to set options
+
+opt.number = true -- Show line numbers
---
+++ lua/plugins.lua
@@ -0,0 +1,37 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- You must run this or `PackerSync` whenever you make changes to your plugin configuration
+-- -- Regenerate compiled loader file
+-- :PackerCompile
+
+-- -- Remove any disabled or unused plugins
+-- :PackerClean
+
+-- -- Clean, then install missing plugins
+-- :PackerInstall
+
+-- -- Clean, then update and install plugins
+-- :PackerUpdate
+
+-- -- Perform `PackerUpdate` and then `PackerCompile`
+-- :PackerSync
+
+-- -- Loads opt plugin immediately
+-- :PackerLoad completion-nvim ale
+
+vim.api.nvim_command("packadd packer.nvim")
+-- returns the require for use in `config` parameter of packer's use
+-- expects the name of the config file
+function get_setup(name)
+  return string.format('require("setup/%s")', name)
+end
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+     config = get_setup("lualine"), -- will this blow up?
+     event = "VimEnter",
+     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+end)
