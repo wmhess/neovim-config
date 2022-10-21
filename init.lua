5b070f7dc833bad65b5b820ef805ba4d9bd11744
wmhess <bill@wmhess.com>
2022-10-21 11:24:36 -0400

    First round of config changes
    Completing the steps in the first part of the tutorial listed at the top of init.lua

---
+++ init.lua
@@ -0,0 +1,26 @@
+-- Following the info @ https://mattermost.com/blog/how-to-install-and-set-up-neovim-for-code-editing/
+
+--[[ init.lua ]]
+-- to source after changes type :luafile % on the command line
+
+
+-- vim.g: maps to vim.api.nvim_set_var; sets global variables. equivalent to :set.
+-- vim.o: maps to vim.api.nvim_win_set_var; sets variables scoped to a given window.
+-- vim.b; maps to vim.api.nvim_buf_set_var; sets variables scoped to a given buffer.
+
+-- vim.go: maps to vim.api.nvim_set_option; equivalent to :setglobal.
+-- vim.bo; maps to vim.api.nvim_buf_set_option; equivalent to :setlocal for buffer options.
+-- vim.wo: maps to vim.api.nvim_win_set_option; equivalent to :setlocal for window options.
+
+
+-- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = " "
+vim.g.localleader = "\\"
+
+-- IMPORTS
+require('vars')      -- Variables
+require('opts')      -- Options
+require('keys')      -- Keymaps
+-- require('plug')      -- Plugins
---
+++ lua/keys.lua
@@ -0,0 +1,6 @@
+--[[ keys.lua ]]
+local map = vim.api.nvim_set_keymap
+
+
+-- remap the key used to leave insert mode
+map('i', 'jk', '<esc>', {})
